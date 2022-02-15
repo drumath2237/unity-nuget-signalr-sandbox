30e851591b47ca41ffe6f062fa5e6811e89a7a75
drumath <2237k.drumath@gmail.com>
2022-02-16 00:32:25 +0900

    :sparkles: signalr系の実装を追加

---
+++ Assets/UnityNuGetSIgnalRTest/Scripts/SignalRClientBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+using UnityEngine;
+
+namespace UnityNuGetSignalRTest
+{
+    public class SignalRClientBehaviour : MonoBehaviour
+    {
+        [SerializeField] private SignalRInfo signalRInfo;
+
+        private HubConnection _connection;
+
+        private async void Start()
+        {
+            if (signalRInfo == null || signalRInfo.apiBaseUrl == "")
+            {
+                return;
+            }
+
+            try
+            {
+                _connection = new HubConnectionBuilder().WithUrl(signalRInfo.apiBaseUrl).Build();
+
+                await _connection.StartAsync();
+
+                _connection.On("event", (string msg) => { Debug.Log($"event message: {msg}"); });
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
+
+            Debug.Log("connected!");
+        }
+
+        public async void SendSignalREventMessage()
+        {
+            if (_connection == null)
+            {
+                return;
+            }
+
+            await _connection.SendAsync("event", "helloooo");
+        }
+
+        private async void OnApplicationQuit()
+        {
+            if (_connection == null) return;
+
+            await _connection.StopAsync();
+            await _connection.DisposeAsync();
+        }
+    }
+}
---
+++ Assets/UnityNuGetSIgnalRTest/Scripts/SignalRInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityNuGetSignalRTest
+{
+    [CreateAssetMenu(fileName = "SignalRInfo", menuName = "SignalRTest/info", order = 0)]
+    public class SignalRInfo : ScriptableObject
+    {
+        public string apiBaseUrl;
+    }
+}
